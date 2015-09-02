a6e46d3c0b504c708544f3f358088a9e2a8e770f
Svante Richter <svante.richter@intendit.se>
2015-09-02 16:20:08 +0200

    Add remoteCache

--- Extension.php
+++ Extension.php
@@ -17,6 +17,7 @@ class Extension extends \Bolt\BaseExtension
         if ($this->app['config']->getWhichEnd()=='frontend') {
             $this->addTwigFunction('numToString', 'numToString');
             $this->addTwigFunction('fileModified', 'fileModified');
+            $this->addTwigFunction('remoteCache', 'remoteCache');
             $this->addTwigFunction('d', 'dumper');
             if(!isset($this->app['config']->get('general')['favicon']) && !file_exists($this->app['resources']->getPath('web')."favicon.ico") || $this->app['config']->get('general/favicon') === false){
                 $this->addSnippet('endofhead', '<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">');
@@ -86,6 +87,19 @@ class Extension extends \Bolt\BaseExtension
     function dumper($variable = "")
     {
         return dump($variable);
+    }
+    
+    function remoteCache($url = "", $time = 604800)
+    {
+        $cachekey = preg_replace("/[^A-Za-z0-9 ]/", '', $url);
+        $resp = $this->app['cache']->fetch("remotecache$cachekey");
+        if ($resp === false) {
+            $curlOptions['CURLOPT_CONNECTTIMEOUT'] = 10;
+            $curlOptions['CURLOPT_USERAGENT'] = "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36";
+            $resp = $this->app['guzzle.client']->get($url, array('headers' =>array('User-Agent' => "Mozilla/5.0 (Windows NT 5.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1866.237 Safari/537.36")), $curlOptions)->getBody(true)->getContents();
+            $this->app['cache']->save("remotecache$cachekey", $resp, $time);
+        }
+        return new \Twig_Markup($resp, 'UTF-8');
     }
     
 }
