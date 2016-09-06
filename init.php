1ec836e6065f58b7f558d9165bc2c682f7dc2401
Alan Smithee <alan.smithee@example.com>
2016-09-06 12:01:37 +0200

    Fix up bolt v3

--- init.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Bolt\Extension\boltabandoned\extras\Extension;
-
-$app['extensions']->register(new Extension($app));
---
+++ src/SahAssarExtrasExtension.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace boltabandoned\boltabandonedExtrasExtension;
+
+use Symfony\Component\VarDumper\VarDumper;
+use Bolt\Extension\SimpleExtension;
+use Bolt\Asset\Snippet\Snippet;
+use Bolt\Controller\Zone;
+use Bolt\Asset\Target;
+
+class boltabandonedExtrasExtension extends SimpleExtension
+{
+    protected function registerTwigPaths()
+    {
+        return [
+            'templates' => ['position' => 'prepend', 'namespace' => 'bolt']
+        ];
+    }
+
+    protected function registerTwigFunctions()
+    {
+        return [
+            'fileModified'    => 'fileModified',
+            'shuffle' => 'twigShuffle',
+            'd' => 'dumper'
+        ];
+    }
+
+    protected function registerAssets()
+    {
+        $asset = new Snippet();
+        $asset->setCallback([$this, 'faviconSnippet'])
+            ->setLocation(Target::AFTER_META)
+            ->setPriority(99)
+            ->setZone(Zone::FRONTEND)
+        ;
+
+        return [
+            $asset,
+        ];
+    }
+
+    public function faviconSnippet()
+    {
+        $app = $this->getContainer();
+        if(!isset($app['config']->get('general')['favicon']) || $app['config']->get('general/favicon') === false){
+            return '<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">';
+        }
+        return '';
+    }
+
+	function twigShuffle($arr) {
+        shuffle($arr);
+        return $arr;
+	}
+
+    function fileModified($file = "")
+    {
+        return filemtime($file);
+    }
+
+    function dumper($variable = "")
+    {
+        return dump($variable);
+    }
+}
